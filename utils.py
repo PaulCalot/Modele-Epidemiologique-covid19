e98859d970f35320377de9b7b78c2d6fdf94907a
PaulCalot <paul.calot@hotmail.fr>
2021-03-02 19:48:32 +0100

    Q1 to 5 done.

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import numpy as np
 from datetime import timedelta # https://docs.python.org/3/library/datetime.html
 
+
+# -------------------------------- X - Input params ------------------------- #
+nb_inputs = 15
 idx_to_keys = {
     0 : 'pa',
     1 : 'pIH',
@@ -18,7 +21,7 @@ idx_to_keys = {
     13 : 'Im0',
     14 : 'lambda1'
 }
-
+key_to_idx = {v : k for k, v in idx_to_keys.items()}
 input_params = {
     'pa': [0.4,0.9],
     'pIH' : [0.05, 0.2],
@@ -37,7 +40,6 @@ input_params = {
     'lambda1' : [10e-4, 10e-3]
 }
 
-
 def draw_input_params(seed = None):
     np.random.seed(seed)
     pa = np.random.uniform(low = 0.4, high = 0.9)
@@ -50,16 +52,6 @@ def draw_input_params(seed = None):
     NU = np.random.uniform(10,20)
     R0 = np.random.uniform(2.9,3.4)
     mu = np.random.uniform(0.01,0.08)
-    
-    # we consider tmin to be the reference of time
-    # each subsequent date is given relatively to tmin
-    #tmax = timedelta(weeks = 4, days = 3)
-    #Nmin = tmax
-    #Nmax = Nmin + timedelta(weeks = 3)
-    #N = Nmin+(1-np.random.uniform(0.0,1.0))*(Nmax-Nmin)
-    #t0 = (1-np.random.uniform(0.0,1.0))*tmax
-    #N = N.total_seconds()/86400
-    #t0 = t0.total_seconds()/86400
     tmin, tmax = 0, 4*7+3 # 24/02/2020 = tmin = 0
     Nmin = tmax
     Nmax = Nmin + 3*7
@@ -83,11 +75,81 @@ def convert(arr):
 def vectorize(delta):
     # goal : returning a delta for each j in [0,14]
     # by default, we'll return delta*[max - min]
-    delta_vect = np.zeros((15))
+    delta_vect = np.zeros((15,15))
     for k in range(15):
         p = input_params[idx_to_keys[k]]
-        delta_vect[k] = delta*(p[1]-p[0])
+        delta_vect[k,k] = delta*(p[1]-p[0])
         #if(type(p[0]) == type(1) and type(p[1]) == type(1)):
         #    delta_vect[k] = int(delta_vect[k])
     return delta_vect
 # for now we don't choose to return int here.
+
+# sampling methods :
+def Monte_Carlo_sampling(R = 100):
+    list_samples = [draw_input_params() for k in range(R)]
+    return np.array(list_samples)
+
+def lhs(R = 100):
+    from pyDOE2 import lhs # https://pythonhosted.org/pyDOE/randomized.html#latin-hypercube
+    lhs = lhs(15 , samples = R) # 15 dimension of the input
+    # everything is in [0,1]
+    
+    # now putting it in the right format
+    
+    for k in range(R):
+        lhs[k] = convert(lhs[k])
+        
+    return lhs
+    
+# -------------------------------------- Y - Output -------------------------- #
+plotting_names = {
+    'S' : 'Susceptible (S)',
+    'Im' : 'Infec. not Detec. (I-)',
+    'Ip' : 'Infec. Detec. (I+)',
+    'Rm' : 'Recov. not Detec. (R-)',
+    'RI' : 'Recov. Detec. (R+)',
+    'H' : 'Hospital (H)',
+    'U' : 'Intensive care (U)',
+    'RH' : 'Recov. Hosp. (R+H)',
+    'D' : 'Dead (D)',
+    'DR' : 'Detected (DR)'
+}
+box_colors = {
+    'S' : 'yellow',
+    'Im' : 'purple',
+    'Ip' : 'orange',
+    'Rm' : 'olive',
+    'RI' : 'olivedrab',
+    'H' : 'lightblue',
+    'U' : 'pink',
+    'RH' : 'darkolivegreen',
+    'D' : 'red',
+    'DR' : 'blue'
+}
+
+ykeys = {
+        'S' : 0,
+        'Im' : 1,
+        'Ip' : 2,
+        'Rm' : 3,
+        'RI' : 4,
+        'H' : 5,
+        'U' : 6,
+        'RH' : 7,
+        'D' : 8,
+        'DR' : 9
+    }
+
+def extract_int_value(solution, step_in_day):
+    # start:end:step
+    sol, period = (solution[::int(1/step_in_day),:], 1) if step_in_day < 1 else (solution, step_in_day) # extraction of integer day value
+    return sol, period # period = 1, if we have a value for each day, if one value every two days, equals 2 etc.
+
+def get_max_load_intensive_care(solution, period):
+    # order : S, Im, Ip, Rm, RI, H, U, RH, D, DR
+    # we suppose that the first element is the 0th day.
+    max_it = np.argmax(solution[:,ykeys['U']])
+    max_U = solution[max_it,ykeys['U']]
+    max_time = max_it*period
+    return np.array([int(max_time), int(max_U)])
+    
---
+++ utils_plotting.py
@@ -0,0 +1,113 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# bokeh plotting
+from bokeh.io import push_notebook, show, output_notebook
+from bokeh.plotting import figure
+from bokeh.layouts import row, column
+from bokeh.models import PrintfTickFormatter
+from bokeh.models import CustomJS, DateRangeSlider
+from bokeh.models import Legend, ColumnDataSource, Label, LabelSet, Range1d
+from bokeh.palettes import magma, viridis
+output_notebook(hide_banner=True)
+
+from ipywidgets import interact, IntSlider, Dropdown, FloatSlider
+
+# --------------------------- Q3) Morris ------------------------- # 
+def plot_morris(mu, sigma, names, verbose = False):
+    if(names == 'default'):
+        names = ['pa', 'pIH', 'pIU', 'pHD', 'pHU', 'pUD', 'NI', 'NH', 'NU', 'R0', 'mu', 'N', 't0', 'Im0', 'lambda1']
+    
+    if(verbose):
+        dtype = [('names', 'S10'), ('mu', float), ('sigma', float)]
+        values = [(n, m, s) for n,m,s in zip(names, mu, sigma)]
+        arr = np.array(values, dtype=dtype)
+        arr = np.sort(arr, order = 'mu')
+        print('Parameters sorted by increasing mu:')
+        pprint(arr)
+        
+    colors = magma(len(mu)) # https://www.geeksforgeeks.org/python-bokeh-colors-class/
+
+    source = ColumnDataSource(data=dict(sigma=sigma,
+                                    mu=mu,
+                                    names=names,
+                                    colors=colors))
+    
+    p = figure(plot_height=450, plot_width=900, title="Morris - sensibility analysis")
+
+        
+    p.scatter(x='mu', y='sigma', size=5, source=source, color ='colors')
+    
+    p.xaxis[0].axis_label = 'Mean'
+    p.yaxis[0].axis_label = 'Standard deviation'
+
+    labels = LabelSet(x='mu', y='sigma', x_offset=2, y_offset=2, text='names', source=source, render_mode='canvas')
+
+    
+    p.add_layout(labels)
+
+    show(p)
+    
+# --------------------------- Q4,5) Sobol  ------------------------- # 
+def plot_sobol(S, St, names= 'default', verbose = False):
+    if(names == 'default'):
+        names = ['pa', 'pIH', 'pIU', 'pHD', 'pHU', 'pUD', 'NI', 'NH', 'NU', 'R0', 'mu', 'N', 't0', 'Im0', 'lambda1']
+    
+    if(verbose):
+        dtype = [('names', 'S10'), ('S', float), ('St', float)]
+        values = [(n, m, s) for n,m,s in zip(names, S, St)]
+        arr = np.array(values, dtype=dtype)
+        arr = np.sort(arr, order = 'St')
+        print('Parameters sorted by increasing $S_t$:')
+        pprint(arr)
+    source = ColumnDataSource(data=dict(S=S,
+                                    St=St,
+                                    names=names))
+    
+    p = figure(x_range = names, plot_height=450, plot_width=900, title="Sobol analysis")
+
+    p.circle(x='names', y='S', size=5, source=source, color='blue', legend_label = 'S')
+    p.circle(x='names', y='St', size=5, source=source, color='red', legend_label = 'St')
+
+    p.xaxis[0].axis_label = 'parameters'
+    p.yaxis[0].axis_label = 'Index value'
+
+    show(p)
+    
+def plot_sobol_time(S, St, names= 'default'):
+    if(names == 'default'):
+        names = ['pa', 'pIH', 'pIU', 'pHD', 'pHU', 'pUD', 'NI', 'NH', 'NU', 'R0', 'mu', 'N', 't0', 'Im0', 'lambda1']
+
+    times = np.arange(0,St.shape[-1],1)
+    
+    p = figure(plot_height=450, plot_width=900, title="Sobol analysis")
+    
+    colors = magma(St.shape[0]) # https://www.geeksforgeeks.org/python-bokeh-colors-class/
+
+    legend_it = []
+
+    for k in range(St.shape[0]):
+        source = ColumnDataSource(data=dict(S=S[k],
+                                        St=St[k],
+                                           times = times))
+        c1 = p.x(x='times', y='S', source=source, color=colors[k], line_width=1.5, alpha=1.0, muted_color=colors[k], muted_alpha=0.01)
+        c2 = p.line(x='times', y='St', source=source, color=colors[k], line_width=1.5, alpha=1.0, muted_color=colors[k], muted_alpha=0.01)
+
+        p.xaxis[0].axis_label = 'parameters'
+        p.yaxis[0].axis_label = 'Index value'
+        
+        legend_it.append((names[k], [c1,c2]))
+        
+    c0 = p.line([],[],color = 'black')
+    c1 = p.x([],[],color='blue')
+    c2 = p.line([],[],color='blue')
+    legend_it.append(('',[c0]))
+    legend_it.append(('S',[c1]))
+    legend_it.append(('St',[c2]))
+    
+    legend = Legend(items=legend_it)
+
+    legend.click_policy="mute"
+    p.add_layout(legend, 'right')
+    
+    show(p)
